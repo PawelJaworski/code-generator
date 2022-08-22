778417ef487f40485b8e50bd67739af51bc5a9d1
paweljaworski <pawel.jaworski@intive.com>
2022-08-22 18:45:03 +0200

    moved out Spring

---
+++ src/main/java/pl/javorek/codegenerator/application/CodeGeneratorImpl.java
@@ -0,0 +1,11 @@
+package pl.javorek.codegenerator.application;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class CodeGeneratorImpl implements CodeGenerator {
+
+  public void generateCode() {
+
+  }
+}
---
+++ src/test/groovy/pl/javorek/codegenerator/application/CodeGenerationTest.groovy
@@ -0,0 +1,11 @@
+package pl.javorek.codegenerator.application
+
+import spock.lang.Specification
+
+class CodeGenerationTest extends Specification {
+    def "first test"() {
+        expect:
+        true
+
+    }
+}
